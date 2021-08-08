53e3d5af7ad9f6f9f98d9c213814ea4eca0e52fa
Luwengier <g217881111@gmail.com>
2021-08-08 21:17:41 +0800

    Try adapting based on props

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles({
   root: {
-    color: 'red',
-    '& p': {
-      margin: 0,
-      color: 'green',
-      '& span': {
-        color: 'blue',
-      },
-    },
+    background: (props) =>
+      props.color === 'red'
+        ? 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)'
+        : 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: (props) =>
+      props.color === 'red'
+        ? '0 3px 5px 2px rgba(255, 105, 135, .3)'
+        : '0 3px 5px 2px rgba(33, 203, 243, .3)',
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+    margin: 8,
   },
 });
 
-export default function App() {
-  const classes = useStyles();
+function MyButton(props) {
+  const { color, ...other } = props;
+  // 使用解構賦值以及其餘運算子，過濾掉props中的color屬性，否則傳入Button會觸發其預設的樣式
+  const classes = useStyles(props);
+  return <Button className={classes.root} {...other} />;
+}
 
+MyButton.propTypes = {
+  color: PropTypes.oneOf(['blue', 'red']).isRequired,
+};
+
+export default function App() {
   return (
-    <div className={classes.root}>
-      This is red since it is inside the root.
-      <p>
-        This is green since it is inside the paragraph{' '}
-        <span>and this is blue since it is inside the span</span>
-      </p>
-    </div>
+    <React.Fragment>
+      <MyButton color="red">Red</MyButton>
+      <MyButton color="blue">Blue</MyButton>
+    </React.Fragment>
   );
 }
